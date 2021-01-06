9efa54ae76b2387175ca20d8085fbb322f852183
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2021-01-06 17:37:55 +0530

    Class containing the base structure of cell of pizza

---
+++ The_PizzaProblem/Cell.java
@@ -0,0 +1,9 @@
+
+public class Cell {
+	
+	int x;  // Representing the X -Cordinates i.e the Row index
+	int y;  // Representing the Y -Cordinates i.e the Column index
+	
+	char ingredient;
+	boolean cutoff = false;  // representing if the cell has been cut out in a slice, default value is false.
+}
