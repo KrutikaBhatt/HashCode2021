d751c00a9cbd24d39dfd6e98a551628ba77da448
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2021-01-16 16:43:41 +0530

    Update Simulation.java

--- Practice Problems/Self Driving Cars/Simulation.java
+++ Practice Problems/Self Driving Cars/Simulation.java
@@ -1,5 +1,6 @@
 package driveRides;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation {
@@ -11,23 +12,32 @@ public class Simulation {
 	int bonus;
 	int time;
 	
-	List<Ride> rides;
+	List<Ride> rides ;
 	List<Vehicle> vehicleList;
 	
 	//Constructor
-	public Simulation(int row,int col,int countVehicle,int countRides,int bonus,int time) {
+	public Simulation(int row,int col,int countVehicle,int countRides,int bonus,int time, List<Vehicle> vehicleList) {
 		this.row = row;
 		this.col = col;
 		this.countVehicle = countVehicle;
 		this.countRides = countRides;
 		this.bonus = bonus;
 		this.time = time;
-		
+		this.vehicleList = vehicleList;
+	
+		// Debug issue :
+		// Issue comes as null pointer exception when addRides as the rides is not initializes
+		//While calling the constructor
+		this.rides = new ArrayList<>(countRides);
 	}
 	
 	public void addRides(Ride ride) {
 		rides.add(ride);
 	}
+	
+	 public List<Ride> getRides() {
+	        return rides;
+	    }
 	
 	
 }
