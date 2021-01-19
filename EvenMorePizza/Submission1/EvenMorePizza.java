6484ef3654e4a5b85a22aaea27c0f072df9092c7
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2021-01-19 19:31:37 +0530

    Create EvenMorePizza.java

---
+++ EvenMorePizza/Submission1/EvenMorePizza.java
@@ -0,0 +1,14 @@
+package evenMorePizza;
+
+public class EvenMorePizza {
+
+	public static void main(String[] args) {
+		
+		String path ="C:\\Users\\User\\Downloads\\"+"e_many_teams.in";
+		MainSolving main = new MainSolving(path);
+		main.ReadInput();
+		
+
+	}
+
+}
